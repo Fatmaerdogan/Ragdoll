7952b802dbbddb0a2934059a846c3192e2a3aac6
Fatmaerdogan <erdoganfatma119@gmail.com>
2020-12-08 11:07:37 +0300

    Ragdoll character
    first loading

---
+++ Ragdoll/Assets/CubeScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    [Tooltip("değişken true ise sağa false ise sola doğru gidecek")]
+    public bool yon;
+    void Update()
+    {
+        if(yon==true)
+            this.transform.position += new Vector3(0f, 0f, 0.1f) ;
+        else
+            this.transform.position -= new Vector3(0f, 0f, 0.1f) ;
+        if (this.transform.position.z >= 5)
+        {
+            yon = false;
+        }
+        else if(this.transform.position.z <= -5)
+        {
+            yon = true;
+        }
+
+    }
+}
